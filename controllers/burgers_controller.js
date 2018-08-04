22feeb36c60f90d0c5e1f02268e4178cdcedda05
Claire Stolp <clairemstolp@gmail.com>
2018-08-03 20:11:26 -0400

    connected front and back end

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,6 @@ const router = express.Router();
 //home
 router.get('/', function (req, res) {
     burger.all(function(results){
-        console.log(results);
 
         results.forEach(function(val){
             if(val.devoured === 1) {
@@ -41,7 +40,6 @@ router.get('/api/burgers/:burger?', function (req, res) {
 router.post('/api/burgers', function (req, res) {
     const newBurger = req.body.burger;
     burger.create(newBurger, function (results) {
-        console.log(results);
         res.json(results);
     });
 });
@@ -51,10 +49,11 @@ router.put('/api/burgers/:id?', function(req, res){
     const condition = 'id=' + req.params.id;
     const obj = {devoured: req.body.devoured}
     burger.update(obj, condition, function (result) {
-        console.log(result);
+        
         if(result.affectedRows === 0){
             res.status(404).send('Cannot find burger');
         }else{
+            console.log('Burger', req.params.id, 'was updated!')
             res.status(200).json(result);
         }
     });
@@ -68,6 +67,7 @@ router.delete('/api/burgers/:id?', function (req, res) {
             console.log(result);
             res.status(404).send('Cannot find burger');
         }else{
+            console.log('Success', req.params.id, 'was deleted');
             res.status(200).json(result);
         }
     });
